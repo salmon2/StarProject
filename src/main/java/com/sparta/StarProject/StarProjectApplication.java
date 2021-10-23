db9553f0377fb23fc776a5d4665e1fba12a241b3
salmon2 <dys04076@naver.com>
2021-10-23 17:51:29 +0900

    first commit

---
+++ src/main/java/com/sparta/StarProject/StarProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.StarProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarProjectApplication.class, args);
+	}
+
+}
