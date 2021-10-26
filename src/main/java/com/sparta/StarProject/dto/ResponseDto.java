49faf477d69b2941148ff0a21bb897460623d6e4
salmon2 <dys04076@naver.com>
2021-10-26 09:32:35 +0900

    박기남 responseDto

---
+++ src/main/java/com/sparta/StarProject/dto/ResponseDto.java
@@ -0,0 +1,17 @@
+package com.sparta.StarProject.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseDto<T> {
+    private Long id;
+    private String msg;
+    private T data;
+}
