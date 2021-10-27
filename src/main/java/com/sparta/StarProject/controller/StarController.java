7f8ae1563961f0b76d750df0640dfe256dbbfe52
min <91645519+7rizL@users.noreply.github.com>
2021-10-27 11:11:26 +0900

    가민 starInfo dto 이름 변경

--- src/main/java/com/sparta/StarProject/controller/StarController.java
+++ src/main/java/com/sparta/StarProject/controller/StarController.java
@@ -1,14 +1,11 @@
 package com.sparta.StarProject.controller;
 
-import com.sparta.StarProject.domain.Location;
 import com.sparta.StarProject.dto.ResponseDto;
-import com.sparta.StarProject.dto.StarResponseDto;
+import com.sparta.StarProject.dto.StarGuideResponseDto;
 import com.sparta.StarProject.service.StarService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class StarController {
@@ -21,9 +18,9 @@ public class StarController {
 
     @GetMapping("/star/info")
     public ResponseDto readStarInfo(@RequestParam("locationId") Long locationId){
-        StarResponseDto starResponseDto = starService.readStarInfo(locationId);
+        StarGuideResponseDto starGuideResponseDto = starService.readStarInfo(locationId);
 
-        return new ResponseDto(200L, "성공", starResponseDto);
+        return new ResponseDto(200L, "성공", starGuideResponseDto);
     }
 
 }
--- src/main/java/com/sparta/StarProject/service/StarService.java
+++ src/main/java/com/sparta/StarProject/service/StarService.java
@@ -2,33 +2,27 @@ package com.sparta.StarProject.service;
 
 import com.sparta.StarProject.domain.Location;
 import com.sparta.StarProject.domain.Star;
-import com.sparta.StarProject.dto.StarResponseDto;
+import com.sparta.StarProject.dto.StarGuideResponseDto;
 import com.sparta.StarProject.repository.LocationRepository;
 import com.sparta.StarProject.repository.StarRepository;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class StarService {
 
-    private final StarRepository starRepository;
     private final LocationRepository locationRepository;
 
-
-    public StarResponseDto readStarInfo(Long locationId) {
+    public StarGuideResponseDto readStarInfo(Long locationId) {
         Optional<Location> findLocation = locationRepository.findById(locationId);
         Location location = findLocation.get();
 
         Star findStar = location.getStar();
 
-        StarResponseDto starResponseDto = new StarResponseDto(
+        StarGuideResponseDto starGuideResponseDto = new StarGuideResponseDto(
                     findStar.getMoonrise(),
                     findStar.getMoonSet(),
                     findStar.getVisibility(),
@@ -37,6 +31,6 @@ public class StarService {
                     findStar.getTemperature()
         );
 
-        return starResponseDto;
+        return starGuideResponseDto;
     }
 }
