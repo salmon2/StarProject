1dd2648c2993155bfcbb4ad1b98dc502e7d16dee
salmon2 <dys04076@naver.com>
2021-11-03 20:48:04 +0900

    배포 0.0.3ver

---
+++ src/main/java/com/sparta/StarProject/config/WebMvcConfigurerimpl.java
@@ -0,0 +1,21 @@
+package com.sparta.StarProject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfigurerimpl implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://leesungmin.shop.s3-website.ap-northeast-2.amazonaws.com/",
+                        "http://localhost:3000",
+                        "http://localhost:8080")
+                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowCredentials(true);
+    }
+
+}
