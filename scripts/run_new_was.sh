b2cf7e900987dd683501c0286c61816d0541361a
salmon2 <dys04076@naver.com>
2021-11-30 22:50:51 +0900

    박기남 스프링 어드민 로그파일 추가

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -22,7 +22,7 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill ${TARGET_PID}
 fi
 
-
+sudo chmode -R 777 /home/ec2-user/star-project
 nohup java -jar -Dserver.port=${TARGET_PORT} /home/ec2-user/star-project/build/libs/StarProject-0.0.1-SNAPSHOT.jar > /home/ec2-user/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
