35b5c4bd405f39f380ec8db988251ebde395d840
salmon2 <dys04076@naver.com>
2021-12-02 17:54:09 +0900

    박기남 script fix

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -23,6 +23,6 @@ if [ ! -z ${TARGET_PID} ]; then
 fi
 
 sudo chmode -R 777 /home/ec2-user/star-project
-nohup java -jar -Dserver.port=${TARGET_PORT} /home/ec2-user/star-project/build/libs/StarProject-0.0.1-SNAPSHOT.jar > /home/ec2-user/nohup.out 2>&1 &
+nohup java -jar -Dserver.port=${TARGET_PORT} /home/ec2-user/star-project/build/libs/* > /home/ec2-user/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
